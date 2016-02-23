fcd9bb16278ec2383a651314adc5fa9d069834da
LudvigSoerensen <ludvigsoerensen@gmail.com>
2016-02-24 00:04:25 +0100

    asd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,14 +79,15 @@ Rails.application.configure do
 
   # Sets paperclip to upload images to Amazon S3
   config.paperclip_defaults = {
+    Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+    Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+    Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+    
     :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-      Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-      Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-      Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
     }
   }
 end
